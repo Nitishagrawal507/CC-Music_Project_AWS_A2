8942b2072548826fa3a58bc4090a3dfb034b70a6
Nitish Agrawal <61539464+Nitishagrawal507@users.noreply.github.com>
2021-05-10 07:48:21 +0530

    Create run.py

---
+++ run.py
@@ -0,0 +1,6 @@
+from flask.templating import render_template
+from assessment2 import assessment2
+
+if __name__ == '__main__':
+    # Run locally
+    assessment2.run(host='127.0.0.1', port=5000, debug=True)
