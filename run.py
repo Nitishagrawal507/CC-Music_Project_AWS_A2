cebd0291795f62715e99c107a424ed70fac7478a
Nitish Agrawal <61539464+Nitishagrawal507@users.noreply.github.com>
2021-05-10 07:48:48 +0530

    Update run.py

--- run.py
+++ run.py
@@ -2,5 +2,4 @@ from flask.templating import render_template
 from assessment2 import assessment2
 
 if __name__ == '__main__':
-    # Run locally
-    assessment2.run(host='127.0.0.1', port=5000, debug=True)
+    assessment2.run(host='127.0.0.1', port=5000, debug=True)#for local
