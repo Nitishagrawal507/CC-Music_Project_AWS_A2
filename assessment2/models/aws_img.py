979e4ca321d6da73f24224079a13b3df3a338c2e
Nitish Agrawal <61539464+Nitishagrawal507@users.noreply.github.com>
2021-05-15 10:45:35 +0530

    Create aws_img.py

---
+++ assessment2/models/aws_img.py
@@ -0,0 +1,13 @@
+from . import s3
+
+MUSIC_IMAGES_BUCKET = 'cc-music-images'
+
+
+def get_image_url(filename: str):
+    response = s3.generate_presigned_url(
+        'get_object',
+        Params={'Bucket': MUSIC_IMAGES_BUCKET, 'Key': filename},
+        ExpiresIn=3600
+    )
+
+    return response
